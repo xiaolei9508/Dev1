3856b2671891bcd431e14ffab51797186ec0a4be
xiaolei <xl818xl@163.com>
2020-04-27 21:12:01 +0800

    3.Coder写下第一行代码

--- src/main/java/TestA.java
+++ src/main/java/TestA.java
@@ -2,5 +2,6 @@ public class TestA {
     public static void main(String[] args) {
         System.out.println("AAA");
         System.out.println("BBB");
+        System.out.println("Code1写下第一行代码");
     }
 }
