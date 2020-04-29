3f46665f503f5c3552880b84bf3de2f6f98dada5
xiaolei <xl818xl@163.com>
2020-04-29 09:16:15 +0800

    4.Coder写下第二行代码

--- src/main/java/TestA.java
+++ src/main/java/TestA.java
@@ -3,5 +3,6 @@ public class TestA {
         System.out.println("AAA");
         System.out.println("BBB");
         System.out.println("Code1写下第一行代码");
+        System.out.println("Code2写下第二行代码");
     }
 }
